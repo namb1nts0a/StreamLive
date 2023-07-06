07f1efc787615642602757529d80ca9e7007bcaf
namb1nts0a <nambitsoanjaratiana@gmail.com>
2023-07-06 23:52:54 +0300

    test login

--- src/main/java/com/StreamLive/stream/controllers/PagesControllers.java
+++ src/main/java/com/StreamLive/stream/controllers/PagesControllers.java
@@ -1,10 +1,22 @@
 package com.StreamLive.stream.controllers;
 
+import com.StreamLive.stream.models.User;
+import com.StreamLive.stream.models.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class PagesControllers {
+
+    private UserRepository userRepository;
+
+    public PagesControllers(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @GetMapping("/")
     public String home(){
         return "index";
@@ -18,6 +30,31 @@ public class PagesControllers {
     @GetMapping("/users")
     public String users(){
         return  "users";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password){
+        if (isValidCredentials(username, password)){
+            System.out.println("Connexion login");
+            return "redirect:/";
+        }else{
+            System.out.println("Connexion error");
+            return "login";
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password){
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if(optionalUser.isPresent()){
+            User user = optionalUser.get();
+            return user.getPassword().equals(password);
+        }
+        return false;
     }
 
 
--- src/main/java/com/StreamLive/stream/models/UserRepository.java
+++ src/main/java/com/StreamLive/stream/models/UserRepository.java
@@ -3,7 +3,10 @@ package com.StreamLive.stream.models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findBySessionId(String sessionId);
+    Optional<User> findByUsername(String username);
 }
